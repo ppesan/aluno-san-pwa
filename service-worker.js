e82d0667c0e50003189dbc0c5f7c1aadc406b9eb
ppesan <ppe.san@iffarroupilha.edu.br>
2026-02-17 09:51:31 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* aluno.san - service-worker.js */
 
-const CACHE_VERSION = "aluno-san-wpa-v1.3"; // ↑ aumente sempre que alterar
+const CACHE_VERSION = "aluno-san-wpa-v1.4"; // ↑ aumente sempre que alterar
 const CACHE_NAME = CACHE_VERSION;
 const CSV_CACHE_NAME = `${CACHE_VERSION}-csv`;
 
@@ -165,5 +165,6 @@ async function staleWhileRevalidate(req, cacheName) {
 
   return cached || fetchPromise;
 }
+
 
 
