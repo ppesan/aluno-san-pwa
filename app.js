e7a628e25d6a9f093f5ce5984b2a8065d2c8f83b
ppesan <ppe.san@iffarroupilha.edu.br>
2026-02-16 23:40:45 -0300

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 */
 
 const SHEET_CSV_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSr4o5yxLQTP-MxL_gBjHC2LqsMbV8LdxlmOUG3VhGVUPMOy9m6n4pCMor4ghtHtDmLOYfkvGdIKCEA/pub?output=csv"; 
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSr4o5yxLQTP-MxL_gBjHC2LqsMbV8LdxlmOUG3VhGVUPMOy9m6n4pCMor4ghtHtDmLOYfkvGdIKCEA/pub?gid=1651715340&single=true&output=csv"; 
 // Exemplo (formato):
 // https://docs.google.com/spreadsheets/d/e/....../pub?gid=XXXX&single=true&output=csv
 
@@ -208,4 +208,5 @@ async function main() {
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
 
